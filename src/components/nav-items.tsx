26c5de77c9855b1513910f0ee8621796d5ae3022
shanesgh <103379145+shanesgh@users.noreply.github.com>
2025-09-24 21:42:20 -0400

    Updated nav-items.tsx

--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -27,17 +27,16 @@ export const NavItemComponent: FC<NavItemProps> = ({
     <div
       onClick={() => navigate({ to: route })}
       className={cn(
-        "relative flex items-center h-9 my-1 px-4 cursor-pointer rounded-lg mx-2",
+        "relative flex items-center h-9 my-1 cursor-pointer rounded-lg mx-2",
         isActive ? "bg-black text-white " : "hover:bg-gray-100 ",
-        isExpanded ? "justify-start" : "justify-center"
+        isExpanded ? "justify-start px-4" : "justify-center px-2"
       )}
       onMouseEnter={() => setShowTooltip(true)}
       onMouseLeave={() => setShowTooltip(false)}
     >
       <Icon className="size-4" />
       {isExpanded && <span className="ml-4 text-sm whitespace-nowrap">{label}</span>}
-      {/* {badge && <Badge className="ml-auto bg-red-500">{badge}</Badge>} */}
-      {!isExpanded && showTooltip && isActive && (
+      {!isExpanded && showTooltip && (
         <div className="absolute left-full ml-2 whitespace-nowrap bg-black text-white text-xs rounded px-2 py-1 z-50">
           {label}
         </div>
